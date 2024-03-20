d1934c07b988cbde9c88cdcc6c0f9ea1c4de783d
Juan Rodriguez <rodrjuan@oregonstate.edu>
2024-03-19 23:34:28 -0700

    Added test for sorting an array of length 10

--- src/lib/sortPoints.test.js
+++ src/lib/sortPoints.test.js
@@ -31,3 +31,33 @@ test("sortPoints() returns input array if elements have same x value (Array leng
     // Assert
     expect(sortPoints(points)).toStrictEqual([{x:10,y:20},{x:10,y:5}])
 })
+
+test("sortPoints() returns a sorted array in ascending order (Array length is 10)", function() {
+    // Arrange
+    points = [
+        {x:5,y:513},
+        {x:3,y:35},
+        {x:61,y:13},
+        {x:713,y:1},
+        {x:10,y:74},
+        {x:1,y:167},
+        {x:642,y:12},
+        {x:62,y:1},
+        {x:1516,y:11},
+        {x:732,y:162}
+            ]
+    // Act
+    // Assert
+    expect(sortPoints(points)).toStrictEqual([
+        {x:1,y:167},
+        {x:3,y:35},
+        {x:5,y:513},
+        {x:10,y:74},
+        {x:61,y:13},
+        {x:62,y:1},
+        {x:642,y:12},
+        {x:713,y:1},
+        {x:732,y:162},
+        {x:1516,y:11}
+            ])
+})
